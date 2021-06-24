c8ef31f89ec997e2bacbb06ee59127f90a2af9b2
Pingachguk <andreitochmatov@gmail.com>
2021-06-24 16:16:27 +0300

    Добавил оплату забронированной тренировки

--- app/Services/Sber.php
+++ app/Services/Sber.php
@@ -4,10 +4,29 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Facades\Http;
+
 class Sber
 {
-    public static function sberRegisterDo($paymentData)
+    public static function sberRegisterDo($item)
     {
+        $itemType = $item['category_type'];
 
+        if ($itemType == 'trainer' || $itemType == 'office') $itemType = 'once';
+
+        $params = [
+            'userName' => 'fitroom-api',
+            'password' => 'Mobifitness*1',
+            # ВКЛЮЧЕН БОЕВОЙ ТОКЕН!!!!!!!!
+            #'token': 'env('SBER_TOKEN')',
+            'amount' => $item['amount'].'00',
+            'returnUrl' => 'https://app.fitroom.ru/success?type='.$itemType.'&subtype='.$item['category_type'],
+            'orderNumber' => $item['orderNumber'],
+            'description' => $item['description'],
+            'phone' => $item['phone']
+        ];
+
+        $response = Http::get('https://securepayments.sberbank.ru/payment/rest/register.do', $params);
+        return $response->json();
     }
 }
